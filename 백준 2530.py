7900d7a29fe07c69ac25a3a06029ce7335278ceb
김진섭 <backtothefuture3030@gmail.com>
2021-06-18 16:15:11 +0900

    인공지능시계

---
+++ 백준 2530.py
@@ -0,0 +1,20 @@
+A, B, C = map(int,input().split())
+D = int(input())
+
+B+=D//60
+C+=D%60
+
+if C>=60:
+    B+=C//60
+    C=C%60
+if B>=60:
+    A+=B//60
+    B=B%60
+
+if A%24==0:
+    A=0
+elif A>24:
+    A=A%24
+
+print(A,B,C)
+    
